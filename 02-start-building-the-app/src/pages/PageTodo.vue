28b8a497dbc7d08bf1ee68dc50c8b7e78d3bd854
martim.lima@jbaysolutions.com <wEGvpdbTZ\8&|C+Q=Qd09=sh>
2021-06-22 14:57:45 +0100

    03 - Create a List of Taskas - Display a List of Tasks

--- 02-start-building-the-app/src/pages/PageTodo.vue
+++ 02-start-building-the-app/src/pages/PageTodo.vue
@@ -1,13 +1,37 @@
 <template>
-  <q-page class="flex flex-center">
-    <p>Todo Page</p>
+  <q-page class="q-pa-md">
+    <q-list bordered>
+      <q-item
+        v-ripple
+        v-for="task in tasks"
+        :key="task.id"
+        @click="!task.completed"
+      >
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.name }}</q-item-label>
+          <q-item-label caption> </q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
-  name: 'PageIndex'
-})
+  name: "PageIndex",
+  data() {
+    return {
+      tasks: [
+        { id: 1, name: "Go to Shop", completed: false },
+        { id: 2, name: "Go to Post Office", completed: false },
+        { id: 3, name: "Go to the Gym", completed: false },
+      ],
+    };
+  },
+});
 </script>
