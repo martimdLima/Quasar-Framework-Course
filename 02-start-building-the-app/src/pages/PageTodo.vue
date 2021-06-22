d30adab61cad557ad862b00cc515e9f66de5ed30
martim.lima@jbaysolutions.com <wEGvpdbTZ\8&|C+Q=Qd09=sh>
2021-06-22 16:02:31 +0100

    04 - Vuex State Management - Setting Up - Create a Getter for the Tasks Data

--- 02-start-building-the-app/src/pages/PageTodo.vue
+++ 02-start-building-the-app/src/pages/PageTodo.vue
@@ -41,35 +41,12 @@
 
 <script>
 import { defineComponent, ref } from "vue";
+import { mapGetters } from "vuex";
 
 export default defineComponent({
   name: "PageIndex",
-  data() {
-    return {
-      tasks: [
-        {
-          id: 1,
-          name: "Go to Shop",
-          dueDate: "22/06/2020",
-          dueTime: "18:30",
-          completed: false,
-        },
-        {
-          id: 2,
-          name: "Go to Post Office",
-          dueDate: "26/06/2020",
-          dueTime: "14:15",
-          completed: false,
-        },
-        {
-          id: 3,
-          name: "Go to the Gym",
-          dueDate: "24/06/2020",
-          dueTime: "09:30",
-          completed: false,
-        },
-      ],
-    };
+  computed: {
+    ...mapGetters("tasks", ["tasks"]),
   },
 });
 </script>
--- 02-start-building-the-app/src/store/store.tasks.js
+++ 02-start-building-the-app/src/store/store.tasks.js
@@ -1,24 +1,43 @@
 const state = {
+  tasks: [
+    {
+      id: 1,
+      name: "Go to Shop",
+      dueDate: "22/06/2020",
+      dueTime: "18:30",
+      completed: true,
+    },
+    {
+      id: 2,
+      name: "Go to Post Office",
+      dueDate: "26/06/2020",
+      dueTime: "14:15",
+      completed: false,
+    },
+    {
+      id: 3,
+      name: "Go to the Gym",
+      dueDate: "24/06/2020",
+      dueTime: "09:30",
+      completed: false,
+    },
+  ],
+};
 
-}
+const mutations = {};
 
-const mutations = {
-
-}
-
-const actions = {
-
-}
-
+const actions = {};
 
 const getters = {
-
-}
+  tasks: (state) => {
+    return state.tasks;
+  },
+};
 
 export default {
-    namespaced: true,
-    state,
-    mutations,
-    actions,
-    getters
-}
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters,
+};
