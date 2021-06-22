bdf7ba79f6fea45df0909feb36b1612e4dc0d0ad
martim.lima@jbaysolutions.com <wEGvpdbTZ\8&|C+Q=Qd09=sh>
2021-06-22 15:17:05 +0100

    03 - Create a List of Taskas - Add Due Date and Time

--- 02-start-building-the-app/src/pages/PageTodo.vue
+++ 02-start-building-the-app/src/pages/PageTodo.vue
@@ -5,7 +5,8 @@
         v-ripple
         v-for="task in tasks"
         :key="task.id"
-        @click="!task.completed"
+        @click="task.completed = !task.completed"
+        clickable
       >
         <q-item-section side top>
           <q-checkbox v-model="task.completed" />
@@ -13,6 +14,22 @@
         <q-item-section>
           <q-item-label>{{ task.name }}</q-item-label>
           <q-item-label caption> </q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <div class="row">
+            <div class="column justify-center">
+              <q-icon name="event" size="18px" class="q-mr-xs" />
+            </div>
+
+            <div class="column">
+              <q-item-label class="row justify-end" caption>{{
+                task.dueDate
+              }}</q-item-label>
+              <q-item-label class="row justify-end" caption>
+                <small>{{ task.dueTime }}</small>
+              </q-item-label>
+            </div>
+          </div>
         </q-item-section>
       </q-item>
     </q-list>
@@ -27,9 +44,27 @@ export default defineComponent({
   data() {
     return {
       tasks: [
-        { id: 1, name: "Go to Shop", completed: false },
-        { id: 2, name: "Go to Post Office", completed: false },
-        { id: 3, name: "Go to the Gym", completed: false },
+        {
+          id: 1,
+          name: "Go to Shop",
+          dueDate: "22/06/2020",
+          dueTime: "18:30",
+          completed: false,
+        },
+        {
+          id: 2,
+          name: "Go to Post Office",
+          dueDate: "26/06/2020",
+          dueTime: "14:15",
+          completed: false,
+        },
+        {
+          id: 3,
+          name: "Go to the Gym",
+          dueDate: "24/06/2020",
+          dueTime: "09:30",
+          completed: false,
+        },
       ],
     };
   },
