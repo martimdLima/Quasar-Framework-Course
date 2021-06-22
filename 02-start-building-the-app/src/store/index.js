49fe396363aadf3b60108a4958c75c27b6f0f357
martim.lima@jbaysolutions.com <wEGvpdbTZ\8&|C+Q=Qd09=sh>
2021-06-22 15:53:43 +0100

    04 - Vuex State Management - Setting Up - Setup a Vuex Store

--- 02-start-building-the-app/src/store/index.js
+++ 02-start-building-the-app/src/store/index.js
@@ -1,5 +1,7 @@
-import { store } from 'quasar/wrappers'
-import { createStore } from 'vuex'
+import { store } from "quasar/wrappers";
+import { createStore } from "vuex";
+
+import tasks from "./store.tasks";
 
 // import example from './module-example'
 
@@ -15,13 +17,13 @@ import { createStore } from 'vuex'
 export default store(function (/* { ssrContext } */) {
   const Store = createStore({
     modules: {
-      // example
+      tasks,
     },
 
     // enable strict mode (adds overhead!)
     // for dev mode and --debug builds only
-    strict: process.env.DEBUGGING
-  })
+    strict: process.env.DEBUGGING,
+  });
 
-  return Store
-})
+  return Store;
+});
