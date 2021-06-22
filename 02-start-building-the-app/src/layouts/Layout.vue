60b32530999552febed3271260cf5aede7297ce0
martim.lima@jbaysolutions.com <wEGvpdbTZ\8&|C+Q=Qd09=sh>
2021-06-22 12:11:11 +0100

    02 - Start Building the App -  Only show Drawer on Desktop, Tabs on Mobile

--- 02-start-building-the-app/src/layouts/Layout.vue
+++ 02-start-building-the-app/src/layouts/Layout.vue
@@ -19,26 +19,44 @@
 
     <q-footer>
       <q-tabs>
-        <q-route-tab v-for="nav in navs" :key="nav.label" :to="nav.to" exact :icon="nav.icon" :label="nav.label" />
+        <q-route-tab
+          v-for="nav in navs"
+          :key="nav.label"
+          :to="nav.to"
+          exact
+          :icon="nav.icon"
+          :label="nav.label"
+        />
       </q-tabs>
     </q-footer>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-grey-1">
+    <q-drawer
+      v-model="leftDrawerOpen"
+      :breakpoint="767"
+      show-if-above
+      bordered
+      class="bg-grey-1"
+    >
       <q-list>
         <q-item-label header class="text-grey-8">
           Essential Links
         </q-item-label>
 
-        <q-item v-for="nav in navs" :key="nav.label" :to="nav.to" exact clickable>
+        <q-item
+          v-for="nav in navs"
+          :key="nav.label"
+          :to="nav.to"
+          exact
+          clickable
+        >
           <q-item-section avatar>
             <q-icon :name="nav.icon" />
           </q-item-section>
 
           <q-item-section>
-            <q-item-label>{{nav.label}}</q-item-label>
+            <q-item-label>{{ nav.label }}</q-item-label>
           </q-item-section>
         </q-item>
-
       </q-list>
     </q-drawer>
 
@@ -66,15 +84,23 @@ export default defineComponent({
         {
           label: "Todo",
           icon: "list",
-          to: "/"
+          to: "/",
         },
-              {
+        {
           label: "Settings",
           icon: "settings",
-          to: "/settings"
-        }
-      ]
+          to: "/settings",
+        },
+      ],
     };
   },
 });
 </script>
+
+<style scoped>
+@media screen and (min-width: 768px) {
+  .q-footer {
+    display: none;
+  }
+}
+</style>
