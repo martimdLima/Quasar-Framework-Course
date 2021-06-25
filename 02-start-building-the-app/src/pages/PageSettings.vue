76ae63817fde36b1e4572bd58fb9288fa23a0319
martim.lima@jbaysolutions.com <wEGvpdbTZ\8&|C+Q=Qd09=sh>
2021-06-25 10:16:01 +0100

    12 - Settings Page - Add a Computed Getter & Setter

--- 02-start-building-the-app/src/pages/PageSettings.vue
+++ 02-start-building-the-app/src/pages/PageSettings.vue
@@ -1,36 +1,40 @@
 <template>
   <q-page padding>
-   <q-list bordered padding>
-
+    <q-list bordered padding>
       <q-item-label header>Settings</q-item-label>
 
       <q-item tag="label" v-ripple>
         <q-item-section>
           <q-item-label>Show 12 hour time format</q-item-label>
         </q-item-section>
-        <q-item-section side >
+        <q-item-section side>
           <q-toggle color="blue" v-model="show12HourTimeFormat" />
         </q-item-section>
       </q-item>
-
     </q-list>
   </q-page>
 </template>
 
 <script>
-import {mapGetters} from "vuex"
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   data() {
-    return {
-      show12HourTimeFormat: false,
-    }
+    return {};
+  },
+  methods: {
+    ...mapActions("settings", ["setShow12HourTimeFormat"]),
   },
   computed: {
-    ...mapGetters("settings", ["getSettings"])
+    ...mapGetters("settings", ["getSettings"]),
+    show12HourTimeFormat: {
+      get() {
+        return this.getSettings.show12HourTimeFormat;
+      },
+      set(value) {
+        this.setShow12HourTimeFormat(value);
+      },
+    },
   },
-  show12HourTimeFormat() {
-    return this.settings.show12HourTimeFormat;
-  }
-}
+};
 </script>
--- 02-start-building-the-app/src/store/store.settings.js
+++ 02-start-building-the-app/src/store/store.settings.js
@@ -3,21 +3,25 @@ import { uid } from "quasar";
 const state = {
   settings: {
     show12HourTimeFormat: false,
-  }
+  },
 };
 
 const mutations = {
-
+  setShow12HourTimeFormat(state, value) {
+    state.settings.show12HourTimeFormat = value;
+  },
 };
 
 const actions = {
-
+  setShow12HourTimeFormat({ commit }, value) {
+    commit("setShow12HourTimeFormat", value);
+  },
 };
 
 const getters = {
-  getSettings: state => {
-    return state.settings
-  }
+  getSettings: (state) => {
+    return state.settings;
+  },
 };
 
 export default {
