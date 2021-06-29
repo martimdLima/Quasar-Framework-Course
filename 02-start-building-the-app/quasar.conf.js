c7a7404bd218c7ebf0104f176f2b7ee28e9bb201
martim.lima@jbaysolutions.com <wEGvpdbTZ\8&|C+Q=Qd09=sh>
2021-06-29 12:27:06 +0100

    14 - Firebase Authentication - Navigation Guards - Create a Boot File

--- 02-start-building-the-app/quasar.conf.js
+++ 02-start-building-the-app/quasar.conf.js
@@ -22,7 +22,8 @@ module.exports = configure(function (ctx) {
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli/boot-files
     boot: [
-      "firebase"
+      "firebase",
+      "router-auth"
     ],
 
     // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
